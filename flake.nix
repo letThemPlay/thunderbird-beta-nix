54bfe5b6a7c2040c7eb8ae76e2fe29f24cf1afa3
kelvin <kelvin.d.morris@gmail.com>
2024-10-29 13:47:54 +0000

    WIP adding update script

--- flake.nix
+++ flake.nix
@@ -28,13 +28,63 @@
       perSystem =
         { pkgs, ... }:
         {
-          packages.default = pkgs.wrapFirefox (pkgs.thunderbird-unwrapped.overrideAttrs rec {
+          packages.thunderbird = pkgs.wrapFirefox (pkgs.thunderbird-unwrapped.overrideAttrs rec {
             version = "132.0b6";
             src = pkgs.fetchurl {
               url = "mirror://mozilla/thunderbird/releases/${version}/source/thunderbird-${version}.source.tar.xz";
               sha256 = "sha256-wRWLOIwnLesfnonav3oGRWiWwCvnfWeu5uC52jtAZkE=";
             };
           }) { libName = "thunderbird"; };
+
+          packages.default =
+            pkgs.wrapThunderbird
+              (pkgs.thunderbird-bin-unwrapped.override {
+                generated = {
+                  version = "132.0b6";
+                  sources = [
+                    {
+                      url = "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/132.0b6/linux-x86_64/en-US/thunderbird-132.0b6.tar.bz2";
+                      arch = "linux-x86_64";
+                      locale = "en-US";
+                      sha256 = "sha256-So0FdYfrd+PzTsW4mQbxyeS5osvKfPUHW4azffvpGKk=";
+                    }
+                  ];
+                };
+              })
+              {
+                applicationName = "thunderbird";
+                pname = "thunderbird-bin";
+                desktopName = "Thunderbird";
+              };
+
+          packages.update =
+            let
+              name = "update";
+              buildInputs = [
+                pkgs.curl
+                pkgs.jq
+                pkgs.jo
+              ];
+              update-script = (pkgs.writeScriptBin name (builtins.readFile ./update.sh)).overrideAttrs (prev: {
+                buildCommand = "${prev.buildCommand}\n patchShebangs $out";
+              });
+            in
+            pkgs.symlinkJoin {
+              inherit name;
+              paths = [ update-script ] ++ buildInputs;
+              buildInputs = [ pkgs.makeWrapper ];
+              postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+            };
+
+          packages.update-versions = pkgs.writeShellScriptBin "update-versions" ''
+            content=$(${pkgs.curl}/bin/curl https://product-details.mozilla.org/1.0/thunderbird_versions.json)
+            beta_version=$( ${pkgs.jq}/bin/jq -r '.LATEST_THUNDERBIRD_DEVEL_VERSION' <<< "$content" )
+
+            url=https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/$beta_version/linux-x86_64/en-US/thunderbird-$beta_version.tar.bz2
+            hash=$(nix-prefetch-url $url)
+
+            jo version="$beta_version" sources=$(jo -a $(jo url="$url" arch="linux-x86_64" locale="en-US" sha256="$hash")) > generated.json
+          '';
         };
     };
 }
---
+++ update.sh
@@ -0,0 +1,13 @@
+locales=("en-GB" "en-US")
+content=$(curl https://product-details.mozilla.org/1.0/thunderbird_versions.json)
+version=$(jq -r '.LATEST_THUNDERBIRD_DEVEL_VERSION' <<< "$content")
+
+sources=()
+
+for locale in ${locales[@]}; do
+  url=https://releases.mozilla.org/pub/thunderbird/releases/$version/linux-x86_64/$locale/thunderbird-$version.tar.bz2
+  hash=$(nix-prefetch-url --unpack $url)
+  sources+=( $(jo url=$url arch="linux-x86_64" locale=$locale sha256=$hash))
+done
+final=$(jo -p version=$version sources=$(jo -a ${sources[@]}))
+echo $final > beta-sources.json
